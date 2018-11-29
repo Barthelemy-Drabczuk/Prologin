bcb89ab5d9dd3dafe8982ae590a1bad4fd1da62f
DRABCZUK Barthelemy <d17010509@a-maq-iutinfo.salsa.univ-amu.fr>
2018-11-29 13:22:01 +0100

    début de l'exo 1

---
+++ exo1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main () {
+
+        unsigned prixInit;
+        unsigned char nbPrix;
+        vector<unsigned> prixInternet;
+
+        cin >> prixInit;
+        
+        
+        return 0;
+}

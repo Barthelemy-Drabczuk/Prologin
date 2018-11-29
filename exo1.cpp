f63eae11e4263063bc3ac27e60c39b5d572c1d20
DRABCZUK Barthelemy <d17010509@a-maq-iutinfo.salsa.univ-amu.fr>
2018-11-29 14:07:49 +0100

    validation de l'exo1

--- exo1.cpp
+++ exo1.cpp
@@ -3,14 +3,36 @@
 
 using namespace std;
 
+bool isArnaque (const unsigned prixInit, const vector<unsigned> & prixInternet) {
+	unsigned nbPrixEnDessous (0);
+	for (unsigned prix : prixInternet) {
+		if (prix < prixInit) {
+			++nbPrixEnDessous;
+		}
+	}
+	return (nbPrixEnDessous >= 3);
+}
+
 int main () {
 
         unsigned prixInit;
-        unsigned char nbPrix;
+        unsigned nbPrix;
         vector<unsigned> prixInternet;
 
         cin >> prixInit;
-        
+	cin >> nbPrix;
+
+	for (unsigned i (0); i < nbPrix; ++i) {
+		unsigned prixInternetActuel;		
+		cin >> prixInternetActuel;
+		prixInternet.push_back (prixInternetActuel);
+	}
+
+	for (unsigned prix : prixInternet) {
+		cout << prix << endl ;
+	}
+
+	(isArnaque (prixInit, prixInternet)) ? cout << "ARNAQUE !" : cout << "Ok bon voyage, bisous, n'oublie pas de m'envoyer des photos !";
         
         return 0;
 }
